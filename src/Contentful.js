eb0314e337125db62637943b8b900c656bccc549
nguyenvy123 <nguyenvyqn21@gmail.com>
2020-09-06 20:56:12 +0700

    second commit

---
+++ src/Contentful.js
@@ -0,0 +1,14 @@
+import {createClient} from "contentful";
+
+export default createClient({
+  space: "1p157jk6zqck",
+  accessToken: "ORmybkeRBBWnzAw5bV1QItiOLti20gYrNNzCexDvvOc"
+});
+
+
+// const contentful = require('contentful')
+//
+// const Client = contentful.createClient({
+//   space: '1p157jk6zqck',
+//   accessToken: 'ORmybkeRBBWnzAw5bV1QItiOLti20gYrNNzCexDvvOc'
+// })
